61b29e9eb2a4be8048ffc6a296998f0bf72b6de4
Jaume <jaume.jurado@alum.esci.upf.edu>
2023-11-10 15:34:28 +0100

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -62,24 +62,89 @@ biobb_structure_checking
 
 
 ####STEP 2
+
+# Importing libraryes:
 import argparse
+import sys
+import os
+import math
 
-# Class / functions needed to calculate VanderWaals parameters or the residue library
+from Bio.PDB.PDBParser import PDBParser
+from Bio.PDB.NACCESS import NACCESS_atomic
+from Bio.PDB.NeighborSearch import NeighborSearch
+from Bio.PDB.PDBIO import PDBIO, Select
 
-# Defining the class
-class ResiduesDataLib:
+# Classes / functions needed to calculate VanderWaals parameters or the residue library
 
-    # Creating a function to initialize the class and use local variables inside the class
+class ResiduesDataLib(): # Creating the class
+
     def __init__(self, fname):
-        self.residues = {}
+        self.residue_data = {}
 
         try:
-            tmtfh = open(fname, "r") # Opening for reading the archive
+            fh = open(fname, "r")
 
-        # Checking for errors, getting error msj and quitting
         except OSError:
-            print(f"# ERROR while loading library file ({fname})")
+            print("#ERROR while loading library file (", fname, ")")
             sys.exit(2)
 
-        for line in 
+        for line in fh:
+
+            if line[0] == '#':
+                continue
+
+            data = line.split()
+            r = Residue(data)
+            self.residue_data[r.id] = r
+
+        self.nres = len(self.residue_data)
+
+    def get_params(self, resid, atid):
+        atom_id = resid + ':' + atid
+
+        if atom_id in self.residue_data:
+            return self.residue_data[atom_id]
         
+        else:
+            print("WARNING: atom not found in library (", atom_id, ')')
+            return None
+
+class Residue():
+    
+    def __init__(self,data):
+        self.id     = data[0]+':'+data[1]
+        self.at_type = data[2]
+        self.charge  = float(data[3])
+        
+class AtomType():
+
+    def __init__(self, data):
+        self.id   = data[0]
+        self.eps  = float(data[1])
+        self.sig  = float(data[2])
+        self.mass = float(data[3])
+        self.fsrf = float(data[4])
+        self.rvdw = self.sig * 0.5612
+        
+class VdwParamset(): #extracted from GELPI's github
+    #parameters for the VdW
+    def __init__ (self, file_name):
+        self.at_types = {}
+
+        try:
+            fh = open(file_name, "r")
+
+        except OSError:
+            print ("#ERROR while loading parameter file (", file_name, ")")
+            sys.exit(2)
+
+        for line in fh:
+
+            if line[0] == '#':
+                continue
+
+            data = line.split()
+            self.at_types[data[0]] = AtomType(data)
+            
+        self.ntypes = len(self.at_types)
+        fh.close()
