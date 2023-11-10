4a7b8b275f732bccc094ec8499cd521a56199346
Jaume <jaume.jurado@alum.esci.upf.edu>
2023-11-10 15:16:48 +0100

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -13,11 +13,14 @@ import biobb_structure_checking.constants as cts
 from biobb_structure_checking.structure_checking import StructureChecking
 base_dir_path=biobb_structure_checking.__path__[0]
 args = cts.set_defaults(base_dir_path,{'notebook':True})
+
 ###Preparation
+
     #1 Obtain the required structure from the PDB
 f=open("./assignment_data/6m0j.pdb", "r")#for executing in visual use absolute path
 parser = PDBParser()
 structure = parser.get_structure("6m0j",f)
+
     #2 Check at PDB which is the composition of a “Biological unit”.
 # Remove all chains but those involved in the biological unit, if necessary
 base_path = './assignment_data/'
@@ -34,17 +37,23 @@ args['debug'] = False
 args['verbose'] = False
 args['coords_only'] = False
 args['overwrite'] = False
+
 #Intializing the checking engine, loading the structure and showing info
 st_c = StructureChecking(base_dir_path, args)
+
     #3 Remove all heteroatoms
 #Remove Hydrogen
 st_c.rem_hydrogen()
+
 #Remove water
 st_c.water("yes")
+
 #Remove metals
 st_c.metals("All")
+
 #Remove ligands
 st_c.ligands("All")
+
     #4
 biobb_structure_checking
 
