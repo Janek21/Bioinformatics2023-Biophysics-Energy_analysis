80c98f724eda3bbd7e4ba13a417a9c00c36ec635
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2023-11-16 15:10:28 +0100

    n

--- step1.2.py
+++ step1.2.py
@@ -2,17 +2,21 @@ from Bio.PDB import *
 parser=PDBParser()
 structure= parser.get_structure("6m0j", "./assignment_data/6m0j_fixed.pdb")
 model =structure[0]
-#chain=model["A"]
-ls=[]
-
+t=0
+chain1_atoms=[]
+chain2_atoms=[]
 #same resseq and/or name in both chains
-#or same position? NO
-#make dict name:resseq
-for c in model:
-    print("For chain ", c)
-    for i in c.get_residues():
-        print(i)
-        i=str(i)
-        i=i.split()
-        i=i[1]
-        ls.append(i)
+for chain in model:
+    if t==0:
+        t=1
+        for residual in chain.get_residues():
+            for atom in residual:
+                chain1_atoms.append(residual)
+    else:
+        for residual in chain.get_residues():
+            for atom in residual:
+                chain2_atoms.append(residual)
+            i=str(i)
+            i=i.split()
+            i=i[1]
+            ls.append(i)
