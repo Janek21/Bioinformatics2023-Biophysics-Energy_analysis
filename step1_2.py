e3d1d42cc57bfa42718d928c2f07f6bec1cc1e98
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2023-11-17 15:11:21 +0100

    Busca d'errors a step1_2(part1)

--- step1_2.py
+++ step1_2.py
@@ -7,8 +7,11 @@ chain1_atoms=[]
 chain2_atoms=[]
 distance_treshold=2.3#placeholder #QUINA ES??
             
-def chain_atoms(structure)
+def chain_atoms(structure):
     model =structure[0]
+    t=0
+    chain1_atoms=[]
+    chain2_atoms=[]
     for chain in model:
         if t==0:
             t=1
@@ -23,6 +26,9 @@ def chain_atoms(structure)
     
 #se q sem ha anat la pinça, l altre manera que he pensat era un while sota els fors de procedure
 #pero primer he pensat en aixo i despres he pensat que aixi quedava mes ordenat
+
+
+#algo raro aqui sotaVVV, s ha de corretgir
 def chain_comparison(chain1_atoms, chain2_atoms):
     interface_residues=set()
     for a1 in chain1_atoms:
