b63e47aadcf55be28f531a43af50b057cca5d5b0
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2023-11-19 10:33:08 +0100

    implementeation of step 1.2 in main script

--- scripts.py
+++ scripts.py
@@ -77,9 +77,9 @@ st_c._save_structure(args['output_structure_path'])
 
 
 ####STEP 1
-from step1_2 import chain_comparison, chain_atoms
-distance_treshold=2.3#QUINA ES?
-chain_atoms(structure)
+from step1_2 import *
+dt=7 #quin es el distance treshold?
+chain_atoms(structure, dt)
 
 
 ####STEP 2
--- step1_2.py
+++ step1_2.py
@@ -1,13 +1,13 @@
 from Bio.PDB import *
 parser=PDBParser()
-structure= parser.get_structure("6m0j", "./assignment_data/6m0j_fixed.pdb")
-model =structure[0]
-t=0
-chain1_atoms=[]
-chain2_atoms=[]
-distance_treshold=2.3#placeholder #QUINA ES??
+#structure= parser.get_structure("6m0j", "./assignment_data/6m0j_fixed.pdb")
+#model =structure[0]
+#t=0
+#chain1_atoms=[]
+#chain2_atoms=[]
+#distance_treshold=2.3#placeholder #QUINA ES??
             
-def chain_atoms(structure):
+def chain_atoms(structure, dt):
     model =structure[0]
     t=0
     chain1_atoms=[]
@@ -22,23 +22,24 @@ def chain_atoms(structure):
             for residual in chain.get_residues():
                 for atom2 in residual:
                     chain2_atoms.append(atom2)
-    return chain_comparison(chain1_atoms, chain2_atoms)
+    return chain_comparison(chain1_atoms, chain2_atoms, dt)
     
     
 #se q sem ha anat la pinça, l altre manera que he pensat era un while sota els fors de procedure
 #pero primer he pensat en aixo i despres he pensat que aixi quedava mes ordenat
 
 
-def chain_comparison(chain1_atoms, chain2_atoms):
+def chain_comparison(chain1_atoms, chain2_atoms, dt):
     interface_residues=set()
     for a1 in chain1_atoms:
         for a2 in chain2_atoms:
             if a1.get_parent().id==a1.get_parent().id and a2.get_parent().id == a2.get_parent().id:
                 distance=a1-a2
-            if distance<=distance_treshold:
+            if distance<=dt:#dt is the treshold of the distance
                 interface_residues.add(a1.get_parent().id[1])
                 interface_residues.add(a2.get_parent().id[1])
     return interface_residues
 
-total_interface_residues=chain_atoms(structure)
-print(f"The interface residues are: {total_interface_residues}")
+if __name__=='__main__':
+    total_interface_residues=chain_atoms(structure)
+    print(f"The interface residues are: {total_interface_residues}")
