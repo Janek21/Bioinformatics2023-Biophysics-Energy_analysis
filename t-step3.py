60d2d7f69d4f09dd701c9e2e15fed6848efc833d
Jaume <jaume.jurado@alum.esci.upf.edu>
2023-11-22 11:36:26 +0100

    update s3

--- t-step3.py
+++ t-step3.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 """
-    Initial setup a structure for Energy evaluation
+	Initial setup a structure for Energy evaluation
 """
 import argparse
 import os
@@ -20,32 +20,32 @@ import step2m2_energies as en
 NACCESS_BINARY = '/home/jaume/Desktop/Year_2/Biophysics/Project:Energy_Analysis/Biophysics_A1/soft/NACCESS/naccess'
 
 parse_cmd = argparse.ArgumentParser(
-    prog='structure_setup',
-    description='basic structure setup'
+	prog='structure_setup',
+	description='basic structure setup'
 )
 
 parse_cmd.add_argument(
-    '--rlib',
-    action='store',
-    dest='reslib_file',
-    default='./assignment_data/parameters_step2.lib',
-    help='Residue Library'
+	'--rlib',
+	action='store',
+	dest='reslib_file',
+	default='./assignment_data/parameters_step2.lib',
+	help='Residue Library'
 )
 parse_cmd.add_argument(
-    '--vdw',
-    action='store',
-    dest='vdwprm_file',
-    default='./assignment_data/parameters_vanderw.txt',
-    help='Vdw parameters'
+	'--vdw',
+	action='store',
+	dest='vdwprm_file',
+	default='./assignment_data/parameters_vanderw.txt',
+	help='Vdw parameters'
 )
 
 parse_cmd.add_argument(
-    '--dist',
-    action='store',
-    dest='cutoff_dist',
-    default=5.0,
-    type=float,
-    help='Cutoff distance for determining the interface (0: use all residues):'
+	'--dist',
+	action='store',
+	dest='cutoff_dist',
+	default=5.0,
+	type=float,
+	help='Cutoff distance for determining the interface (0: use all residues):'
 )
 parse_cmd.add_argument('pdb_file', help='Input PDB', type=open)
 
@@ -88,26 +88,26 @@ io = PDBIO()
 st_chains = {}
 # Using BioIO trick (see tutorial) to select chains
 class SelectChain(Select):
-    def __init__(self, chid):
-        self.id = chid
+	def __init__(self, chid):
+		self.id = chid
 
-    def accept_chain(self, chain):
-        if chain.id == self.id:
-            return 1
-        else:
-            return 0
+	def accept_chain(self, chain):
+		if chain.id == self.id:
+			return 1
+		else:
+			return 0
 
 for ch in st[0]:
-    io.set_structure(st)
-    io.save('tmp.pdb', SelectChain(ch.id))
-    st_chains[ch.id] = parser.get_structure('stA', 'tmp.pdb')
-    en.add_atom_parameters(st_chains[ch.id], residue_library, ff_params)
-    srfA = NACCESS_atomic(st_chains[ch.id][0], naccess_binary=NACCESS_BINARY)
+	io.set_structure(st)
+	io.save('tmp.pdb', SelectChain(ch.id))
+	st_chains[ch.id] = parser.get_structure('stA', 'tmp.pdb')
+	en.add_atom_parameters(st_chains[ch.id], residue_library, ff_params)
+	srfA = NACCESS_atomic(st_chains[ch.id][0], naccess_binary=NACCESS_BINARY)
 os.remove('tmp.pdb')
 
 ## Interface residues
 if args.cutoff_dist > 0.:
-    interface = en.get_interface(st, args.cutoff_dist)
+	interface = en.get_interface(st, args.cutoff_dist)
 
 ## Initiatlize Energy aggregates
 elec = {}
@@ -129,35 +129,35 @@ totalSolvMon = {}
 ## We get the chsin ids,not always they are A and B
 chids = []
 for ch in st[0]:
-    chids.append(ch.id)
-    totalSolvMon[ch.id] = 0
+	chids.append(ch.id)
+	totalSolvMon[ch.id] = 0
 
 total = 0.
 
 
 print(f'\nInteraction energy based in interface residues ONLY')
 for ch in st[0]:
-    for res in ch.get_residues():
-        if args.cutoff_dist > 0 and res not in interface[ch.id]:
-            continue
-        elec[res], elec_ala[res], vdw[res], vdw_ala[res] = en.calc_int_energies(st[0], res)
-        solvAB[res], solvAB_ala[res] = en.calc_solvation(st[0], res)
-        solvA[res], solvA_ala[res] = en.calc_solvation(
-            st_chains[ch.id],
-            st_chains[ch.id][0][ch.id][res.id[1]]
-        )
-        totalIntElec += elec[res]
-        totalIntVdw += vdw[res]
-        totalSolv += solvAB[res]
-        totalSolvMon[ch.id] += solvA[res]
-        total += elec[res] + vdw[res] + solvAB[res] - solvA[res]
-        print(
-            'D#{:11} {:11.4f}{:11.4f}{:11.4f}{:11.4f} - {:11.4f}{:11.4f}{:11.4f}{:11.4f}'.format(
-                en.residue_id(res),
-                elec[res], vdw[res], solvAB[res], solvA[res],
-                elec_ala[res], vdw_ala[res], solvAB_ala[res], solvA_ala[res]
-            )
-        )
+	for res in ch.get_residues():
+		if args.cutoff_dist > 0 and res not in interface[ch.id]:
+			continue
+		elec[res], elec_ala[res], vdw[res], vdw_ala[res] = en.calc_int_energies(st[0], res)
+		solvAB[res], solvAB_ala[res] = en.calc_solvation(st[0], res)
+		solvA[res], solvA_ala[res] = en.calc_solvation(
+			st_chains[ch.id],
+			st_chains[ch.id][0][ch.id][res.id[1]]
+		)
+		totalIntElec += elec[res]
+		totalIntVdw += vdw[res]
+		totalSolv += solvAB[res]
+		totalSolvMon[ch.id] += solvA[res]
+		total += elec[res] + vdw[res] + solvAB[res] - solvA[res]
+		print(
+			'D#{:11} {:11.4f}{:11.4f}{:11.4f}{:11.4f} - {:11.4f}{:11.4f}{:11.4f}{:11.4f}'.format(
+				en.residue_id(res),
+				elec[res], vdw[res], solvAB[res], solvA[res],
+				elec_ala[res], vdw_ala[res], solvAB_ala[res], solvA_ala[res]
+			)
+		)
 print(f'\nTOTAL ENERGIES of interaction energy based in interface residues ONLY')
 print('{:20}: {:11.4f}'.format('Total Elec Int.', totalIntElec))
 print('{:20}: {:11.4f}'.format('Total Vdw Int.', totalIntVdw))
@@ -169,41 +169,67 @@ print("")
 
 print("Ala Scanning: DDGs for X->Ala mutations on interface residues")
 print(
-    '{:11} {:11s}{:11s}{:11s}{:11s}{:11s}'.format(
-        'res_id',
-        'elec',
-        'vdw',
-        'solvAB',
-        'solv',
-        'total'
-    )
+	'{:11} {:11s}{:11s}{:11s}{:11s}{:11s}'.format(
+		'res_id',
+		'elec',
+		'vdw',
+		'solvAB',
+		'solv',
+		'total'
+	)
 )
 
 for ch in st[0]:
-    for res in ch.get_residues():
-        if args.cutoff_dist > 0 and res not in interface[ch.id]:
-            continue
-        print(
-            '{:11} {:11.4f}{:11.4f}{:11.4f}{:11.4f}{:11.4f}'.format(
-                en.residue_id(res),
-                - elec[res] + elec_ala[res],
-                - vdw[res] + vdw_ala[res],
-                - solvAB[res] + solvAB_ala[res],
-                - solvA[res] + solvA_ala[res],
-                - elec[res] + elec_ala[res] - vdw[res] + vdw_ala[res] -solvAB[res] +\
-                    solvAB_ala[res] -solvA[res] + solvA_ala[res]
-            )
-        )
-        
+	for res in ch.get_residues():
+		if args.cutoff_dist > 0 and res not in interface[ch.id]:
+			continue
+		print(
+			'{:11} {:11.4f}{:11.4f}{:11.4f}{:11.4f}{:11.4f}'.format(
+				en.residue_id(res),
+				- elec[res] + elec_ala[res],
+				- vdw[res] + vdw_ala[res],
+				- solvAB[res] + solvAB_ala[res],
+				- solvA[res] + solvA_ala[res],
+				- elec[res] + elec_ala[res] - vdw[res] + vdw_ala[res] -solvAB[res] +\
+					solvAB_ala[res] -solvA[res] + solvA_ala[res]
+			)
+		)
+
+print(f'\nWritting in file: ala_scaning.tsv')
+with open("ala_scaning.tsv", "w") as file:
+	file.write(
+		'{:11} {:11s}{:11s}{:11s}{:11s}{:11s}'.format(
+		'res_id',
+		'elec',
+		'vdw',
+		'solvAB',
+		'solv',
+		'total'))
+	for ch in st[0]:
+		for res in ch.get_residues():
+			if args.cutoff_dist > 0 and res not in interface[ch.id]:
+				continue
+			file.write(
+				'{:11} {:11.4f}{:11.4f}{:11.4f}{:11.4f}{:11.4f}'.format(
+					en.residue_id(res),
+					- elec[res] + elec_ala[res],
+		   		 - vdw[res] + vdw_ala[res],
+		  		  - solvAB[res] + solvAB_ala[res],
+		  		  - solvA[res] + solvA_ala[res],
+		  		  - elec[res] + elec_ala[res] - vdw[res] + vdw_ala[res] -solvAB[res] +\
+		  			  solvAB_ala[res] -solvA[res] + solvA_ala[res]
+		  	  )
+	  	  )
+
 '''
 # Calculate the  effect of replacing each residue by an alanine
 with open("ala_scanning.txt", "a") as alanine:
-    for ch in st[0]:
-        for res in ch.get_residues():
-            if max_dist > 0 and res not in interface[ch.id]:
-                continue
-            print('{:1}, {:1.4}'.format(residue_id(res), 
-                    - elec[res] + elec_ala[res] - vdw[res] + vdw_ala[res] - solvAB[res] +\
-                        solvAB_ala[res] -solvA[res] + solvA_ala[res]), file = alanine)
+	for ch in st[0]:
+		for res in ch.get_residues():
+			if max_dist > 0 and res not in interface[ch.id]:
+				continue
+			print('{:1}, {:1.4}'.format(residue_id(res), 
+					- elec[res] + elec_ala[res] - vdw[res] + vdw_ala[res] - solvAB[res] +\
+						solvAB_ala[res] -solvA[res] + solvA_ala[res]), file = alanine)
 '''
 
