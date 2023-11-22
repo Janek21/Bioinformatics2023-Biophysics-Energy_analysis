7f0d1d0d7dd98a13f576ad096e2eb18eb75018ee
DavidMarquez <david.marquez@alum.esci.upf.edu>
2023-11-22 21:42:01 +0100

    step 1 updated to scripts.py

--- scripts.py
+++ scripts.py
@@ -77,8 +77,37 @@ st_c._save_structure(args['output_structure_path'])
 
 
 ####STEP 1
-from step1_2 import *
-dt=5 #quin es el distance treshold?
+#finding out atoms in both chains
+def chain_atoms(structure, dt):
+	model = structure[0]
+	t = 0
+	chain1_atoms = []
+	chain2_atoms = []
+	for chain in model:
+		if t==0:
+			t=1
+			for residual in chain.get_residues():
+				for atom1 in residual:
+					chain1_atoms.append(atom1)
+		else:
+			for residual in chain.get_residues():
+				for atom2 in residual:
+					chain2_atoms.append(atom2)
+	return chain_comparison(chain1_atoms, chain2_atoms, dt)
+
+#comparing atoms of both chains
+def chain_comparison(chain1_atoms, chain2_atoms, dt):
+    interface_residues=set()
+    for a1 in chain1_atoms:
+        for a2 in chain2_atoms:
+            if a1.get_parent().id==a1.get_parent().id and a2.get_parent().id == a2.get_parent().id:
+                distance=a1-a2
+            if distance<=dt:#dt is the treshold of the distance
+                interface_residues.add(a1.get_parent().id[1])
+                interface_residues.add(a2.get_parent().id[1])
+    return interface_residues
+
+dt=5 #distance treshold
 chain_atoms(structure, dt)
 
 
